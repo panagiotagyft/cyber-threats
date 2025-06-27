b71737d51a2987c74e83b4f29716d88918d171fa
panagiotagyft <giotagiota20001@gmail.com>
2025-06-27 23:14:41 +0300

    messages

--- src/pages/presentation/Presentation.js
+++ src/pages/presentation/Presentation.js
@@ -7,35 +7,40 @@ const images = [
   process.env.PUBLIC_URL + '/assets/team_photos/panagiotis.png',
 ];
 
+const messages = [
+  'The scrollytelling was built with Observable',
+  'The website was built with the React framework',
+  'The dashboard was built with Tableau',
+];
+
 export default function Presentation() {
   const [current, setCurrent] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrent(prev => (prev + 1) % images.length);
-    }, 10000); // change every 10 seconds
+    }, 5000); // change every 5 seconds
     return () => clearInterval(interval);
   }, []);
 
   return (
-
     <div className='presentation_container'>
-        <div className="title-contenair-pr">
-            <h1 className="pr-title0">Presentation</h1>
-        </div>
+      <div className="title-contenair-pr">
+        <h1 className="pr-title0">Presentation</h1>
+      </div>
 
-        <div className="video-sticker-container">
-
+      <div className="video-sticker-container">
         <div className="stickers">
-            <div className="speech-bubble">hello there!!!!!!!!</div>
-            <img
+          <div className="speech-bubble">{messages[current]}</div>
+          <img
             src={images[current]}
             alt={`sticker ${current + 1}`}
             className="sticker"
-            />
+          />
         </div>
+
         <div className="video-wrapper">
-            <iframe
+          <iframe
             width="560"
             height="315"
             src="https://www.youtube.com/embed/TwCUITIZuow"
@@ -43,9 +48,9 @@ export default function Presentation() {
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
-            />
+          />
         </div>
-        </div>
+      </div>
     </div>
   );
 }
